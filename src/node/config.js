ba6ab41c85f13dd229736d83325d7d8fdca2e11a
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2018-10-19 13:24:36 +0300

    wip: headers & crypto

--- src/node/config.js
+++ src/node/config.js
@@ -1,5 +1,10 @@
-const forks = {
+export const forks = {
   two: 9000,
   four: 26900,
   seven: 28135,
+}
+
+export const params = {
+  retarget_frequency: 2000,
+  initial_difficulty: 8844,
 }
--- src/node/headers.js
+++ src/node/headers.js
@@ -1,5 +1,7 @@
+import Big from 'big-integer'
+
 import {hash} from './crypto.js'
-import {forks} from './config.js'
+import {forks, params} from './config.js'
 import {string_to_array, array_to_string, integer_to_array} from './format.js'
 
 function hash2integer(hash, offset) {
@@ -14,9 +16,8 @@ function hash2integer(hash, offset) {
   }
 
   function dec2bin(dec) {
-    const n = dec.toString(2)
+    var n = dec.toString(2)
     n = '00000000'.substr(n.length) + n
-
     return n
   }
 
@@ -58,26 +59,20 @@ function hash2integer(hash, offset) {
   return hash2integer2(hash.concat([255]), 0, 0)
 }
 
-function bigInt(n) {
-  return n
+function log2(x) {
+  if (x.eq(Big.zero)) {
+    return Big.one
+  } else if (x.eq(Big.one)) {
+    return Big.one
+  } else {
+    return Big.one.plus(log2(x.divide(Big(2))))
+  }
 }
 
-function log2(x) {
-  if (x.eq(0)) {
-    return 1
-  } else if (x.eq(1)) {
-    return 1
-  }
-  //if (x == 1) { return 1; }
-  else {
-    return 1 + log2(x.divide(2))
-  }
-  //else { return 1 + log2(Math.floor(x / 2))}
-}
 function exponent(a, b) {
   //a is type bigint. b is an int.
   if (b == 0) {
-    return bigInt(1)
+    return Big.one
   } else if (b == 1) {
     return a
   } else if (b % 2 == 0) {
@@ -87,8 +82,38 @@ function exponent(a, b) {
   }
 }
 
-function sci2int(x) {
-  return x
+export function sci2int(x) {
+  function pair2int(a, b) {
+    const c = exponent(Big(2), a) //c is a bigint
+    return c.times(256 + b).divide(256)
+    //return Math.floor((c * (256 + b)) / 256);
+  }
+
+  function sci2pair(i) {
+    var a = Math.floor(i / 256)
+    var b = i % 256
+    return [a, b]
+  }
+  return pair2int(...sci2pair(x))
+}
+
+export function int2sci(x) {
+  function pair2sci(a, b) {
+    return 256 * a + b
+  }
+  function int2pair(x) {
+    var a = log2(x) - 1
+    var c = exponent(Big(2), a)
+    var b = Number(
+      x
+        .times(256)
+        .divide(c)
+        .minus(256),
+    )
+    //var b = Math.floor((x * 256) / c) - 256;
+    return [a, b]
+  }
+  return pair2sci(...int2pair(x))
 }
 
 export default class Headers {
@@ -98,6 +123,21 @@ export default class Headers {
     this.headers = {}
 
     this.height = 28001
+
+    const top_header = [
+      'header',
+      28001,
+      'f3PfnlxML/UPF9T5ixy1+Q539NyOVfFG07x4pf3zw6Q=',
+      '4A7MYFe5u7OG22QGUvIFguzZWYWndkZARGdImbhbEjM=',
+      'huIlyyrALPoafVluEL/ZYtZ8BXHUJEPxcXCLid5CSnU=',
+      141617794,
+      14053,
+      3,
+      'AAAAAAAAAAAA6ZeG6UQ+dPE+8iEbHoY92if6pIMAAlI=',
+      193346798808507350000,
+      5982,
+    ]
+    this.writeHeader(top_header, 1865656952131054)
   }
 
   syncHeaders = async () => {
@@ -121,17 +161,43 @@ export default class Headers {
     }
   }
 
+  difficultyShouldBe(next_header, prev_header_hash) {
+    const header = this.readHeader(prev_header_hash)
+    if (header === undefined) {
+      console.log('Received an orphan header: ' + prev_header_hash)
+      return 'unknown parent'
+    } else {
+      const diff = header[6]
+      const RF = params.retarget_frequency
+      const height = header[1]
+
+      let x
+      if (height > forks.four) {
+        x = height % Math.floor(RF / 2)
+      } else {
+        x = height % RF
+      }
+
+      const prev_ewah = this.readEWAH(prev_header_hash)
+      const ewah = this.calcEWAH(next_header, header, prev_ewah)
+
+      if (height > forks.seven) {
+        return [this.newTarget(header, ewah), ewah]
+      } else if (x === 0 && !(height < 10)) {
+        return [this.difficultyShouldBe2(header), ewah]
+      } else {
+        return [diff, ewah]
+      }
+    }
+  }
+
   checkPoW(header) {
     const height = header[1]
-
-    if (height < 2) {
-      return {valid: true, ewah: 1000000}
-    } else {
+    if (height < 2) return {valid: true, ewah: 1000000}
+    else {
       const previous_hash = string_to_array(atob(header[2]))
-      const {diff0, ewah} = difficulty_should_be(header, previous_hash)
-
+      const {diff0, ewah} = this.difficultyShouldBe(header, previous_hash)
       const diff = header[6]
-
       if (diff === diff0) {
         const nonce = atob(header[8])
 
@@ -184,7 +250,11 @@ export default class Headers {
   }
 
   readHeader(header_hash) {
-    return this.headers[header_hash][0]
+    if (this.headers[header_hash] !== undefined) {
+      return this.headers[header_hash][0]
+    } else {
+      return undefined
+    }
   }
 
   writeHeader(header, ewah) {
@@ -193,9 +263,16 @@ export default class Headers {
     this.headers[header_hash] = [header, ewah]
   }
 
-  absorbHeader = function(header) {
-    const {valid, ewah} = this.checkPoW(header)
+  readEWAH(header_hash) {
+    if (this.headers[header_hash] !== undefined) {
+      return this.headers[header_hash][1]
+    } else {
+      return undefined
+    }
+  }
 
+  absorbHeader(header) {
+    const {valid, ewah} = this.checkPoW(header)
     if (valid) {
       const height = header[1]
       const header_hash = hash(this.serializeHeader(header))
@@ -204,11 +281,10 @@ export default class Headers {
         header[9] = 0
       } else {
         const prev_hash = this.string_to_array(atob(header[2]))
-        const prev_header = header //this.read_header(prev_hash);
+        const prev_header = this.readHeader(prev_hash)
         const prev_accum_diff = prev_header[9]
         const diff = header[6]
         const accum_diff = sci2int(diff)
-
         header[9] = prev_accum_diff + accum_diff - 1
       }
 
