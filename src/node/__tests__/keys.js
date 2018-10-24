0a37d0ede62671d7ccfc53afa57a85839ca6f5c2
Oleg Kislitsyn <olegafx@gmail.com>
2018-10-25 01:37:52 +0300

    refactor(tests/keys): extract inline keyPair data to defaults

--- src/node/__tests__/keys.js
+++ src/node/__tests__/keys.js
@@ -2,6 +2,11 @@ import Keys from '../keys';
 
 const defaults = {
   entropy: 'This is test entropy used to create test data',
+  keyPair: {
+    private: 'c29ec7d3a8d514c88128e48a59c15f13841058056850109c2ea49b1b3af1bca8',
+    public:
+      '0456964975f83d52661b85998b4b971f5b5dc6f73cf8a45da09b3504543a189caeaceb135d3c0b0f098327eb4735030c93a92427c09d56ca5e13d6dec4ce740a33',
+  },
 };
 
 test('should set non-empty elliptic curve', () => {
@@ -33,35 +38,20 @@ test('should return keyPair on getKeyPair()', () => {
   const keys = new Keys(defaults.entropy);
 
   const keyPair = keys.getKeyPair();
-
-  const expectedKeyPair = {
-    private: 'c29ec7d3a8d514c88128e48a59c15f13841058056850109c2ea49b1b3af1bca8',
-    public:
-      '0456964975f83d52661b85998b4b971f5b5dc6f73cf8a45da09b3504543a189caeaceb135d3c0b0f098327eb4735030c93a92427c09d56ca5e13d6dec4ce740a33',
-  };
+  const expectedKeyPair = { ...defaults.keyPair };
 
   expect(keyPair).toEqual(expectedKeyPair);
 });
 
 test('should load private key', () => {
-  const params = {
-    ...defaults,
-    entropy: 'Random entropy to generate random key pair',
-  };
+  const entropy = 'Random entropy to generate random key pair';
 
-  const keys = new Keys(params);
-
-  keys.setPrivateKey(
-    'c29ec7d3a8d514c88128e48a59c15f13841058056850109c2ea49b1b3af1bca8',
-  );
+  const keys = new Keys(entropy);
+  keys.setPrivateKey(defaults.keyPair.private);
 
   const keyPair = keys.getKeyPair();
 
-  const expectedKeyPair = {
-    private: 'c29ec7d3a8d514c88128e48a59c15f13841058056850109c2ea49b1b3af1bca8',
-    public:
-      '0456964975f83d52661b85998b4b971f5b5dc6f73cf8a45da09b3504543a189caeaceb135d3c0b0f098327eb4735030c93a92427c09d56ca5e13d6dec4ce740a33',
-  };
+  const expectedKeyPair = { ...defaults.keyPair };
 
   expect(keyPair).toEqual(expectedKeyPair);
 });
