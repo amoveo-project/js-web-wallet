2393b37954ff2879abd2a85ec6641e60a2853135
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2018-12-12 11:00:50 +0300

    packaging framework

---
+++ package/build_deb.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BRANCH=${1:-master}
+mkdir -p debian/var/www/html/amoveo-wallet/app >/dev/null 2>&1
+rm -rf debian/var/www/html/amoveo-wallet/*
+
+VERSION=$(git describe --always --tags | sed -re "s/-/~${BRANCH}+/" | sed -re 's/^[^0-9]//g')
+
+(
+    cd ..
+    rm -rf build
+    mkdir build
+    yarn build
+)
+
+fpm -s dir -t deb \
+    -n exantech-amoveo-wallet \
+    -v ${VERSION} \
+    -a all \
+    --prefix=/ \
+    ../build/=/var/www/html/amoveo-wallet/
