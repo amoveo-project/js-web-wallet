b6a64fc788dbb0860adce0de4a274c456b72f9e5
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2018-12-12 11:11:06 +0300

    fix packaging

--- package/build_deb.sh
+++ package/build_deb.sh
@@ -4,12 +4,15 @@ BRANCH=${1:-master}
 mkdir -p debian/var/www/html/amoveo-wallet/app >/dev/null 2>&1
 rm -rf debian/var/www/html/amoveo-wallet/*
 
-VERSION=$(git describe --always --tags | sed -re "s/-/~${BRANCH}+/" | sed -re 's/^[^0-9]//g')
+VERSION=$(git describe --always --tags | sed -re "s%-%~${BRANCH}+%" -re "s%/%-%g" | sed -re 's/^[^0-9]//g')
 
 (
     cd ..
+
     rm -rf build
     mkdir build
+
+    yarn
     yarn build
 )
 
