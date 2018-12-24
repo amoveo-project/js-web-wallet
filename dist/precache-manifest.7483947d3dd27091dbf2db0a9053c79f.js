f84643503e13f4b9c90395e906ebc72c26f0c8e0
Denis Voskvitsov <denis.voskvitsov@gmail.com>
2018-12-24 20:30:54 +0300

    prepare to publish

---
+++ dist/precache-manifest.7483947d3dd27091dbf2db0a9053c79f.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e9fae7d47cd9c578efddcce61f7804f",
+    "url": "/static/media/icon-send.0e9fae7d.svg"
+  },
+  {
+    "revision": "7c73b894aa71d9a38f95",
+    "url": "/static/js/main.7c73b894.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5181cfefefd8bdc37f27587341cb38ee",
+    "url": "/static/media/OCRAExtended.5181cfef.eot"
+  },
+  {
+    "revision": "f03e359a223b95195fea90a4764a060d",
+    "url": "/static/media/OCRAExtended.f03e359a.woff"
+  },
+  {
+    "revision": "0502c214fa63aff2b79311c3066ebd01",
+    "url": "/static/media/OCRAExtended.0502c214.woff2"
+  },
+  {
+    "revision": "e1b8184a9a5e341191f8d9cf2a0e2329",
+    "url": "/static/media/logo.e1b8184a.svg"
+  },
+  {
+    "revision": "ff65aed1ae948eb4f1d2fa5c347da7e6",
+    "url": "/static/media/icon-next.ff65aed1.svg"
+  },
+  {
+    "revision": "13daa668e183f9086c64ee579e2088e4",
+    "url": "/static/media/icon-prev.13daa668.svg"
+  },
+  {
+    "revision": "c3f06eeaaf2d7ad64a7c74b05ba8d257",
+    "url": "/static/media/logo-circle.c3f06eea.svg"
+  },
+  {
+    "revision": "4c0d8e1293ece29e788f42233398112f",
+    "url": "/static/media/icon-download.4c0d8e12.svg"
+  },
+  {
+    "revision": "c28ec58fe534aaf071850df1e17fc3e2",
+    "url": "/static/media/icon-restore.c28ec58f.svg"
+  },
+  {
+    "revision": "a39b1af0f155a0a34fafda2d1151c59a",
+    "url": "/static/media/icon-receive.a39b1af0.svg"
+  },
+  {
+    "revision": "665c4cea6d91184ee8dea6c2779ae3f6",
+    "url": "/static/media/icon-pending.665c4cea.svg"
+  },
+  {
+    "revision": "935fea7fd304337fe4f49a7e817457fe",
+    "url": "/static/media/icon-clipboard.935fea7f.svg"
+  },
+  {
+    "revision": "6bfc5e3ffa3659cc60cebb146149894f",
+    "url": "/static/media/icon-arrow-left.6bfc5e3f.svg"
+  },
+  {
+    "revision": "a14d4a46bf56ad22effba94838290122",
+    "url": "/static/media/icon-arrow-right.a14d4a46.svg"
+  },
+  {
+    "revision": "c0eac2a0201b09b98c794feac6dd3917",
+    "url": "/static/media/icon-gear.c0eac2a0.svg"
+  },
+  {
+    "revision": "073d3e50c5c4ab87d768ed618073d932",
+    "url": "/static/media/icon-exchange.073d3e50.svg"
+  },
+  {
+    "revision": "6e9d3ec5046bc439a21ac8e0cf0f01f0",
+    "url": "/static/media/icon-arrow-down.6e9d3ec5.svg"
+  },
+  {
+    "revision": "e60cb77cd442165b18ef7dff6cb47256",
+    "url": "/static/media/icon-wallet.e60cb77c.svg"
+  },
+  {
+    "revision": "e74bbc924cf7933c3800",
+    "url": "/static/js/1.e74bbc92.chunk.js"
+  },
+  {
+    "revision": "e54fbde9d8868ef8f9e8ab96d40a193c",
+    "url": "/index.html"
+  }
+];
