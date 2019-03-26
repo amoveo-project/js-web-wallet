08db1d9ae777570641e4ca58db47e06b74b60fab
Oleg Kislitsyn <olegafx@gmail.com>
2019-03-26 19:09:00 +0300

    chore(Faq): add test example

--- src/screens/Faq/index.js
+++ src/screens/Faq/index.js
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from '@reach/router';
 
 import FaqContext from 'shared/contexts/FaqContext';
 
@@ -11,8 +12,18 @@ const questionGroups = [
     items: [
       {
         question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
-        answer:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        answer: (
+          <Fragment>
+            Lorem ipsum dolor <Link to="/test">Test</Link> sit amet, consectetur
+            adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </Fragment>
+        ),
         id: 1,
       },
       {
