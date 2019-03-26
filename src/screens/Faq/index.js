623de391b61af7b15f0c879df653da7da85ca8ee
Oleg Kislitsyn <olegafx@gmail.com>
2019-03-26 18:33:20 +0300

    chore(Faq): improve toggle question logic

--- src/screens/Faq/index.js
+++ src/screens/Faq/index.js
@@ -72,14 +72,10 @@ const questionGroups = [
 ];
 
 const FaqContainer = () => {
-  const [openedQuestions, setOpenedQuestions] = useState([]);
+  const [openedQuestions, setOpenedQuestions] = useState([1]);
 
   function handleToggleQuestion(questionId) {
-    const arr = openedQuestions.includes(questionId)
-      ? openedQuestions.filter(id => id !== questionId)
-      : [...openedQuestions, questionId];
-
-    setOpenedQuestions(arr);
+    setOpenedQuestions([questionId]);
   }
 
   const faqState = {
